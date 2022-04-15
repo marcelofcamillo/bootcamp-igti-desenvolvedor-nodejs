439c706778c24833b2de3c4d8d561decfef4a120
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-15 18:03:37 -0300

    my-bank-api: método POST

--- Módulo 1 - Desenvolvimento Back End com JavaScript/construcao-api/my-bank-api/routes/account.js
+++ Módulo 1 - Desenvolvimento Back End com JavaScript/construcao-api/my-bank-api/routes/account.js
@@ -1,5 +1,29 @@
 import express from 'express';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
 
 const router = express();
+
+router.post('/', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = await JSON.parse(await readFile(global.fileName));
+
+    account = {
+      id: data.nextId++,
+      name: account.name,
+      balance: account.balance,
+    };
+
+    data.accounts.push(account);
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.send(account);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
 
 export default router;
