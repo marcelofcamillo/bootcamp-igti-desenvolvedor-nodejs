d423b752edd64e62391d2c4eda537fd36b249a4a
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-16 12:42:12 -0300

    my-bank-api: método PUT

--- Módulo 1 - Desenvolvimento Back End com JavaScript/construcao-api/my-bank-api/routes/account.js
+++ Módulo 1 - Desenvolvimento Back End com JavaScript/construcao-api/my-bank-api/routes/account.js
@@ -65,4 +65,20 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index] = account;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.status(200).send(account);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
 export default router;
