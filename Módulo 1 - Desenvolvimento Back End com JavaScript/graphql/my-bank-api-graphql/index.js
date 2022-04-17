dee65c1dc1aafd3f9fdff6cdfc6cdc875917d4e4
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-16 23:32:24 -0300

    GraphQL my-bank-api: criando as mutations

--- Módulo 1 - Desenvolvimento Back End com JavaScript/graphql/my-bank-api-graphql/index.js
+++ Módulo 1 - Desenvolvimento Back End com JavaScript/graphql/my-bank-api-graphql/index.js
@@ -32,9 +32,20 @@ const schema = buildSchema(`
     name: String
     balance: Float
   }
+  input AccountInput {
+    id: Int
+    name: String
+    balance: Float
+  }
   type Query {
     getAccounts: [Account]
     getAccount(id: Int): Account
+  }
+  type Mutation {
+    createAccount(account: AccountInput): Account
+    deleteAccount(id: Int): Boolean
+    updateAccount(account: AccountInput): Account
+    updateBalance(account: AccountInput): Account
   }
 `);
 
@@ -42,6 +53,15 @@ const root = {
   getAccounts: () => AccountService.getAccounts(),
   getAccount(args) {
     return AccountService.getAccount(args.id);
+  },
+  createAccount({ account }) {
+    return AccountService.createAccount(account);
+  },
+  deleteAccount(args) {
+    AccountService.deleteAccount(args.id);
+  },
+  updateBalance({ account }) {
+    return AccountService.updateBalance(account);
   },
 };
 
