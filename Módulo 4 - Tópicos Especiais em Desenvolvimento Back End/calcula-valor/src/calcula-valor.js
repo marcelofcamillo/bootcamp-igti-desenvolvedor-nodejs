08559b713d2fc25486ba8cfab3843786299058a1
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-05-08 22:02:05 -0300

    calcula-valor: finalização dos testes unitários

--- Módulo 4 - Tópicos Especiais em Desenvolvimento Back End/calcula-valor/src/calcula-valor.js
+++
@@ -1,8 +0,0 @@
-function calcularMontante(capital, taxa, periodo) {
-  // M = C(1 + i)^t
-  let montante = capital * Math.pow(1 + taxa, periodo - 1);
-
-  return montante;
-}
-
-module.exports = { calcularMontante };
